893063c408188c9da80efb7a70920f9a4f4fabec
Pedro Mello <pedromellogomes@gmail.com>
2021-06-27 17:44:59 -0300

    reorganização dos arquivos CSS

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
 
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
 
         <?php wp_head(); ?>
     </head>
