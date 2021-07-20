9aad2749895fa0dce14f6d5263bb42f0a6b8aef7
Pedro Mello <pedromellogomes@gmail.com>
2021-07-20 01:14:30 -0300

    footer refeito

--- footer.php
+++ footer.php
@@ -1,16 +1,17 @@
-        <footer class="footer">
-            <div class="container">
+        <footer class="footer grid grid-cols-1 md:grid-cols-3 justify-items-center items-center p-4">
+            <div class="p-6">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoGEM.png" alt="Logo do GEM" class="w-24 h-auto"/>
+            </div>
 
-                <a class="footer__logo" href="#">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/gem_logo.jpg" alt="Logo do GEM" class="w-24 h-auto"/>
-                </a>
-            
-                <div class="footer__social">
+            <div class="flex justify-around md:pt-24">
+                &copy; Copyright 2021 Grupo Economia do Mar
+            </div>
+
+            <div>
+                <div class="footer_faleconosco font-bold py-2">FALE CONOSCO</div>
+                <div class="flex justify-around">
                     <a href="#">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/icon/facebook.svg" hight="20px" width="20px" />
-                    </a>
-                    <a href="#">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/icon/twitter.svg" hight="20px" width="20px" />
                     </a>
                     <a href="#">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/icon/linkedin.svg" hight="20px" width="20px" />
@@ -19,27 +20,8 @@
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/icon/instagram.svg" hight="20px" width="20px" />
                     </a>
                 </div>
-            
-                <div class="footer__links col1">
-                    <a href="#">About Us</a>
-                    <a href="#">Contact</a>
-                    <a href="#">Blog</a>
-                </div>
-            
-                <div class="footer__links col2">
-                    <p>Fale conosco</p>
-                    <p>grupoeconomiadomar@gem.com</p>
-                </div>
-            
-                <div class="footer__cta">
-                    <a href="#" class="button">Assine a newsletter</a>
-                </div>
-            
-                <div class="footer__copyright">
-                    &copy; Grupo de Economia do Mar. All Rights Reserved.
-                </div>
-
             </div>
+
         </footer>
 
         <script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js"></script>
