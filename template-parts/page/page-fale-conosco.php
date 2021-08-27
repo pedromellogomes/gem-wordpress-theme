114150be170d41d36ef956b486af9d6ec615b2ee
Pedro Mello <pedromellogomes@gmail.com>
2021-08-27 16:13:07 -0300

    Página fale-conosco (#8)
    * remove comment
    
    * fale-conosco assets
    
    * add page-assine.php and header link
    
    * page fale-conosco styled
    
    * bug fix and duplicated filed deleted
    
    * add fale-conosco form style

---
+++ template-parts/page/page-fale-conosco.php
@@ -0,0 +1,40 @@
+<?php
+    global $IMG_DIR;
+?>
+
+<?php get_header(); ?>
+
+<main class="grid grid-cols-1 md:grid-cols-2 gap-x-8 justify-items-center">
+    <section class="background-reverso font-clara md:justify-self-end">
+        <div class="p-6 md:px-20 md:my-20">
+            <div class="text-3xl font-semibold text-center my-10">
+                Fale com o GEM
+            </div>
+            <?php echo do_shortcode( '[wpforms id="35"]' ); ?>
+        </div>
+    </section>
+    <section class="md:justify-self-start md:flex md:items-center">
+        <div class="text-center md:px-20">
+            <img src="<?php echo $IMG_DIR; ?>/fale-conosco/gem_grupos.svg" class="md:w-96 m-6" alt="">
+            <div class="px-20">
+                Nossas redes sociais:
+                <div class="flex justify-around my-4">
+                    <a href="">
+                        <img src="<?php echo $IMG_DIR; ?>/fale-conosco/facebook.svg" class="h-6" alt="">
+                    </a>
+                    <a href="">
+                        <img src="<?php echo $IMG_DIR; ?>/fale-conosco/instagram.svg" class="h-6" alt="">
+                    </a>
+                    <a href="">
+                        <img src="<?php echo $IMG_DIR; ?>/fale-conosco/linkedin.svg" class="h-6" alt="">
+                    </a>
+                    <a href="">
+                        <img src="<?php echo $IMG_DIR; ?>/fale-conosco/email.svg" class="h-6" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
