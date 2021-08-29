f7d7657fb2a5745512ec33aee0ed0a9e583ff652
henriqueborgesv <henriquevasconcellos@id.uff.br>
2021-08-28 21:30:31 -0300

    links nos icones

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
             <div class="col-sm newsletter">
                 <h5 style="font-weight: bold;">FIQUE POR DENTRO DO MUNDO DA ECONOMIA DO <span style="color: #004b6a;">MAR</span></h5>
                 <p style="font-size: small; padding: 0px;">Assine nossa newsletter e receba também nosso policy brief mensal</p>
-                <?php echo do_shortcode( '[sibwp_form id=4]' ); ?>
+                <?php echo do_shortcode('[sibwp_form id=4]'); ?>
                 <!--
                 <form>
                     <div class="form-group row">
@@ -68,100 +68,67 @@
 
 <section id="subgrupos">
     <h3>Subgrupos</h3>
+    <?php
+    $categories = get_terms('category', ['hide_empty' => 0 ]);
+    ?>
     <div class="row">
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/bluefinance.svg"" alt=""></a>
-            <p>BLUE FINANCE</p>
+        <?php
+        foreach ($categories as $term) :
+            $category = get_term($term->term_id);
+            if($category->slug!="sem-categoria"):
+        ?>
+            <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
+                <a href="<?php echo get_term_link($category->term_id); ?>"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/<?php echo $category->slug; ?>.svg"" alt=""></a>
+            <p><?php echo $category->name; ?></p>
         </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/meioambiente.svg"" alt=""></a>
-            <p>CLIMA E MEIO AMBIENTE</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/metodos.svg"" alt=""></a>
-            <p>CONCEITOS E MÉTODOS</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/reparos.svg"" alt=""></a>
-            <p>CONSTRUÇÃO E REPARAÇÃO NAVAL</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/seguranca.svg"" alt=""></a>
-            <p>DEFESA E SEGURANÇA</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/energia.svg"" alt=""></a>
-            <p>ENERGIAS OFFSHORE</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/minerios.svg"" alt=""></a>
-            <p>MINÉRIOS OFFSHORE</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/pesca.svg"" alt=""></a>
-            <p>PESCA E AQUACULTURA</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/geopolitica.svg"" alt=""></a>
-            <p>RELAÇÕES GEOPOLÍTICAS</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/relacoessociais.svg"" alt=""></a>
-            <p>RELAÇÕES SOCIAIS</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/transporte.svg"" alt=""></a>
-            <p>TRANSPORTE E INFRAESTRUTURA</p>
-        </div>
-        <div class="col-6 col-sm-4 col-md-2 logoSubgrupos">
-            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/Subgrupos/SVG/com_fundo/turismo.svg"" alt=""></a>
-            <p>TURISMO, ESPORTE E LAZER</p>
-        </div>
+    <?php endif;
+    endforeach;
+    ?>
     </div>
 </section>
 
-<section id="ultimasPublicacoes">
-    <div class="flex py-12">
-        <div class="w-2/3">
-            <div class="flex flex-wrap content-center">
-                <div class="text-side-box w-8 mr-2"></div>
-                <h3>ULTIMAS PUBLICAÇÕES</h3>
-            </div>
-            <div class="flex text-center p-6">
-                <?php
-                    $posts = get_posts([
-                        'post_status' => 'publish',
-                        'numberposts' => 5
-                    ]);
-                    foreach ($posts as $post) :
-                        setup_postdata( $post );
-                ?>
-                <div class="h-1/4 p-6 m-2">
-                    <div class="h-44 w-36 m-2 rounded-md bg-gray-300">
-                        thumbnail
-                    </div>
-                    <a href="<?php the_permalink(); ?>" class="">
-                        <?php the_title(); ?>
-                    </a>
-                </div>
-                <?php
-                    endforeach;
-                    wp_reset_postdata();
-                ?>
-            </div>
-        </div>
+<section id=" ultimasPublicacoes">
+                    <div class="flex py-12">
+                        <div class="w-2/3">
+                            <div class="flex flex-wrap content-center">
+                                <div class="text-side-box w-8 mr-2"></div>
+                                <h3>ULTIMAS PUBLICAÇÕES</h3>
+                            </div>
+                            <div class="flex text-center p-6">
+                                <?php
+                                $posts = get_posts([
+                                    'post_status' => 'publish',
+                                    'numberposts' => 5
+                                ]);
+                                foreach ($posts as $post) :
+                                    setup_postdata($post);
+                                ?>
+                                    <div class="h-1/4 p-6 m-2">
+                                        <div class="h-44 w-36 m-2 rounded-md bg-gray-300">
+                                            thumbnail
+                                        </div>
+                                        <a href="<?php the_permalink(); ?>" class="">
+                                            <?php the_title(); ?>
+                                        </a>
+                                    </div>
+                                <?php
+                                endforeach;
+                                wp_reset_postdata();
+                                ?>
+                            </div>
+                        </div>
 
-        <div class="linha-vertical"></div>
-        <div class="w-1/3">
-            <div class="flex flex-col justify-center px-32">
-                <div>
-                    <h3 class="text-center">EQUIPE</h3>
-                </div>
-                <p class="text-justify text-black">O GEM nasceu de uma iniciativa individual do Prof. Dr. Thauan Santos (EGN), que identificou a ausência de um grupo sólido de pesquisa no contexto brasileiro sobre a temática em questão. Após mapeamento dos(as) interessados, e consideradas as diversidades regional, de gênero, de formação acadêmica e de experiência profissional, cada pesquisador(a) foi contatado(a) para a formação da equipe.</p>
-                <a href="http://">Saiba mais</a>
-            </div>
-        </div>
-    </div>
+                        <div class="linha-vertical"></div>
+                        <div class="w-1/3">
+                            <div class="flex flex-col justify-center px-32">
+                                <div>
+                                    <h3 class="text-center">EQUIPE</h3>
+                                </div>
+                                <p class="text-justify text-black">O GEM nasceu de uma iniciativa individual do Prof. Dr. Thauan Santos (EGN), que identificou a ausência de um grupo sólido de pesquisa no contexto brasileiro sobre a temática em questão. Após mapeamento dos(as) interessados, e consideradas as diversidades regional, de gênero, de formação acadêmica e de experiência profissional, cada pesquisador(a) foi contatado(a) para a formação da equipe.</p>
+                                <a href="http://">Saiba mais</a>
+                            </div>
+                        </div>
+                    </div>
 </section>
 
 
